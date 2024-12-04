05d7ebe4ae875e962c90f7d010f1b6dd2421c294
diegoperezfoscal <diegoperez.foscal@gmail.com>
2024-12-04 18:03:18 -0500

    correcionpara el deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Plans from './components/Plans';
-import WhyUs from './components/WhyUs';
 import Reviews from './components/Reviews';
 import Contact from './components/Contact';
 import Slidert from './components/Slidert';
